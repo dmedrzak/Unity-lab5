58d245827c06c4bc99b114568b408f70ce9020fa
Daniel Mędrzak <Daniel Mdrzak>
2017-05-17 11:59:53 +0200

    lab5 mecanim

---
+++ lab5/Assets/CapsuleTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleTrigger : MonoBehaviour {
+    public GameObject DoorGameObject;
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = DoorGameObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("isOpen", true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("isOpen", false);
+        }
+    }
+}
